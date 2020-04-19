3a2d2c209c01abf2de488fac729f871c56841c7c
Developer Jahid <jhpabel@gmail.com>
2020-04-19 14:28:51 +0600

    tags

--- static/cms/cms.js
+++
@@ -1,7 +0,0 @@
-//file relation widgets
-import cms from 'netlify-cms-app'
-import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
- 
-cms.registerWidget(FileRelationWidget)
- 
-cms.init()
