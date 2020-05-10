0378b3602eaefbe1eca85b87b74aa8633ecdb4d3
Developer Jahid <jhpabel@gmail.com>
2020-05-10 12:12:48 +0600

    minhaj post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,73 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 import "bootstrap/dist/css/bootstrap.min.css"
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardDeck,
+  CardBody,
+  CardSubtitle,
+} from "reactstrap"
 import "../styles/app.css"
 import Layout from "../components/layout/layout"
 
-const IndexPage = () => (
-  <Layout>
-    <div className="container">
-      <div className="hero-header">
-        <div className="headline">Hi! I'm Delog</div>
-        <div className="primary-content">
-          I have been specifically designed to become a digital home for
-          designers and developers, help them build amazing professional looking
-          websites with ease. You don't have to worry about nitty gritty of web
-          hosting services to run a blog and yet take full advantage of CMS to
-          manage content :)
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <div className="container">
+        <div className="hero-header">
+          <div className="headline">Hi! I'm Delog</div>
+          <div className="primary-content">
+            I have been specifically designed to become a digital home for
+            designers and developers, help them build amazing professional
+            looking websites with ease. You don't have to worry about nitty
+            gritty of web hosting services to run a blog and yet take full
+            advantage of CMS to manage content :)
+          </div>
+          <Link className="button -primary" to="/contact">
+            Get in touch →
+          </Link>
         </div>
-        <a className="button -primary" href="/contact">
-          Get in touch →
-        </a>
+        <CardDeck className="post">
+          {posts.map(post => (
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src={post.node.frontmatter.image}
+                alt={post.node.frontmatter.title}
+              />
+              <CardBody>
+                <CardTitle>{post.node.frontmatter.title}</CardTitle>
+                <CardSubtitle>{post.node.frontmatter.date}</CardSubtitle>
+              </CardBody>
+            </Card>
+          ))}
+        </CardDeck>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query indexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            tags
+            path
+            image
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
